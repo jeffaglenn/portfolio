052b7c6078b010fa9103a4e6bda11ca1963c2ddf
Jeff Glenn <jeff@2dimes.com>
2025-04-25 09:00:46 -0500

    Homepage & Animation updates

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -62,27 +62,27 @@ function revertToOriginalImage() {
 // --------------------------------- //
 // Glow Effect & Mousemove Handling
 // --------------------------------- //
-const glowContainer = document.querySelector(".glow");
-const { left, top } = glowContainer.getBoundingClientRect();
-const centerX = left + 62.5; // 125 / 2 - 125 is the width of the glow
-const centerY = top + 62.5;  // 125 / 2 - 125 is the height of the glow
-const maxDistance = 5;
-const maxDistanceFromCenter = 200;
+// const glowContainer = document.querySelector(".glow");
+// const { left, top } = glowContainer.getBoundingClientRect();
+// const centerX = left + 62.5; // 125 / 2 - 125 is the width of the glow
+// const centerY = top + 62.5;  // 125 / 2 - 125 is the height of the glow
+// const maxDistance = 5;
+// const maxDistanceFromCenter = 200;
 
 document.addEventListener("mousemove", ({ clientX, clientY }) => {
     // Calculate distance and angle from container center
-    const dx = centerX - clientX;
-    const dy = centerY - clientY;
-    const distance = Math.min(
-        (Math.sqrt(dx * dx + dy * dy) / maxDistanceFromCenter) * maxDistance,
-        maxDistance
-    );
-    const angle = Math.atan2(dy, dx);
+    // const dx = centerX - clientX;
+    // const dy = centerY - clientY;
+    // const distance = Math.min(
+    //     (Math.sqrt(dx * dx + dy * dy) / maxDistanceFromCenter) * maxDistance,
+    //     maxDistance
+    // );
+    // const angle = Math.atan2(dy, dx);
 
     // Move the glow container
-    glowContainer.style.transform = `translate(${Math.cos(angle) * distance}px, ${
-        Math.sin(angle) * distance
-    }px)`;
+    // glowContainer.style.transform = `translate(${Math.cos(angle) * distance}px, ${
+    //     Math.sin(angle) * distance
+    // }px)`;
 
     // Change profile pic if the mouse has moved > 100px in any direction
     if (Math.abs(clientX - lastMouseX) > 100 || Math.abs(clientY - lastMouseY) > 100) {
@@ -105,13 +105,77 @@ const buttonHover = (e) => {
     const { currentTarget: target } = e;
 
     const rect = target.getBoundingClientRect(),
-        x = e.clientX - rect.left;
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
 
     target.style.setProperty('--button-mouse-x', `${ x }px`);
+    target.style.setProperty('--button-mouse-y', `${ y }px`);
 }
 
 if (!isTouchDevice()) {
-    for(const button of document.querySelectorAll('.btn')) {
+    for(const button of document.querySelectorAll('.glow-hover')) {
         button.onmousemove = (e) => buttonHover(e)
     }
 }
+
+
+
+
+
+import { animate, onScroll, utils, stagger, createAnimatable } from 'animejs';
+
+animate('[data-stagger-fade-in]', {
+    y: {
+        from: '10rem',
+    },
+    opacity: {
+        from: 0,
+    },
+    delay: stagger(100),
+    autoplay: onScroll({
+        enter: 'bottom-=200 top',
+    })
+});
+
+utils.$('[data-fade-in]').forEach(item => {
+    animate(item, {
+        y: {
+            from: '10rem',
+        },
+        opacity: {
+            from: 0,
+        },
+        autoplay: onScroll({
+            enter: 'bottom-=200 top',
+        })
+    });
+});
+
+// utils.$('[data-project]').forEach(item => {
+//     let bounds = item.getBoundingClientRect();
+
+//     const refreshBounds = () => bounds = item.getBoundingClientRect();
+//     let image = item.querySelector('img');
+
+//     const animatableImage = createAnimatable(image, {
+//         x: 500,
+//         y: 500,
+//         ease: 'out(3)',
+//     });
+//     const onMouseMove = e => {
+//         const { width, height, left, top } = bounds;
+//         const hw = width / 2;
+//         const hh = height / 2;
+//         const x = utils.clamp(e.clientX - left - hw, -hw, hw);
+//         const y = utils.clamp(e.clientY - top - hh, -hh, hh);
+//         animatableImage.x(x);
+//         animatableImage.y(y);
+//     }
+
+//     window.addEventListener('mousemove', onMouseMove);
+//     project.addEventListener('scroll', refreshBounds);
+// });
+
+// const project = document.querySelector('[data-project]');
+
+

e52e50a31ab9882e36381e3f97a01cd6c588a08e
Jeff Glenn <jeff@2dimes.com>
2025-04-25 08:58:26 -0500

    Defines Collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,44 @@
-import { file } from "astro/loaders"
 import { defineCollection, z } from "astro:content";
+import { file, glob } from "astro/loaders"
+
+const projects = defineCollection({
+    loader: file('src/content/projects.json'),
+    schema: ({image}) => z.object({
+        title: z.string(),
+        description: z.string(),
+        url: z.string(),
+        image: z.object(
+            {
+                src: image(),
+                alt: z.string(),
+            }
+        ),
+        technologies: z.array(z.enum([
+            'Craft CMS',
+            'Alpine.js',
+            'TailwindCSS',
+            'Astro.js',
+            'GSAP',
+            'Wordpress',
+            'Algolia',
+        ])),
+    })
+});
+
+const experience = defineCollection({
+    loader: glob({
+        pattern: "**/*.md",
+        base: "./src/content/experience",
+    }),
+    schema: z.object({
+        position: z.string(),
+        company: z.string(),
+        companyUrl: z.string(),
+        dates: z.string()
+    })
+});
 
 export const collections = {
-    projects: defineCollection({
-        loader: file("src/content/projects.json"),
-        schema: z.object({
-            title: z.string(),
-            description: z.string(),
-            url: z.string(),
-            technologies: z.array(z.string()),
-        })
-    }),
+    projects,
+    experience
 }
